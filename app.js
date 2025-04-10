c00b6fd5dcd5f49cf29a37f8e3a92b5876230ff4
CiaranMccarthy1 <ciaranmccarthy100@gmail.com>
2025-04-10 16:05:24 +0100

    Got a sphere to rotate and added shadow

--- app.js
+++ app.js
@@ -13,20 +13,41 @@ var renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setClearColor("#000000");
 
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
 
 document.body.appendChild( renderer.domElement );
+   
+const geometry = new THREE.SphereGeometry(1, 32, 32); 
+const material = new THREE.MeshStandardMaterial({ color: 0x0077ff }); 
+const sphere = new THREE.Mesh(geometry, material);
+sphere.castShadow = true; 
+scene.add(sphere); 
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-var cube = new THREE.Mesh( geometry, material );
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.5 });  
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = - Math.PI / 2;  
+plane.position.y = -2; 
+plane.receiveShadow = true;  
+scene.add(plane);  
 
-scene.add( cube );
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);  
+light.castShadow = true;  
+scene.add(light);  
+
+
+light.shadow.mapSize.width = 512;  
+light.shadow.mapSize.height = 512; 
+light.shadow.camera.near = 0.5; 
+light.shadow.camera.far = 10; 
 
 var render = function () {
   requestAnimationFrame( render );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
 
 
   renderer.render(scene, camera);
