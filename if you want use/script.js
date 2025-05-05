745715e20c1ade048375465c5a2a7b68bbcfe9f3
EmpireStateRJMcG03 <richard9mcgrath@gmail.com>
2025-05-05 17:15:49 +0100

    Add files via upload
    this was the web sit that I was working on. if any of these can be usefully to you

---
+++ if you want use/script.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector('.menu-button')
+const Thor = document.querySelector('.Thor')
+menuButton.addEventListener('click', function(){
+    Thor.classList.toggle('active')
+})
